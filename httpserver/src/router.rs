ad7a7512c88f66d955c23bd9fb9d509ef8867a44
compar <81157921@qq.com>
2022-11-04 09:58:01 +0800

    完成示例

---
+++ httpserver/src/router.rs
@@ -0,0 +1,37 @@
+use std::io::Write;
+
+use http::{httprequest,httprequest::HttpRequest,httpresponse::HttpResponse};
+use super::handler::{WebServiceHandler, PageNotFoundHandler,StaticPageHandler,Handler};
+
+
+
+
+pub struct  Route{
+    
+}
+impl Route {
+    pub(crate) fn route(req: HttpRequest, stream: &mut impl Write) ->() {
+        match  req.method {
+            httprequest::Method::Get =>
+                match &req.resource {
+                    httprequest::Resource::Path(s) =>{
+                        let route:Vec<&str> = s.split("/").collect();
+                        match route[1] {
+                            "api" =>{
+                                let resp:HttpResponse = WebServiceHandler::handle(&req);
+                                let _=resp.send_response(stream);
+                            }
+                            _=> {
+                                let resp:HttpResponse = StaticPageHandler::handle(&req); 
+                                let _=resp.send_response(stream);
+                            }
+                        }
+                    }
+                },
+                 _ =>{
+                    let resp:HttpResponse = PageNotFoundHandler::handle(&req);
+                    let _=resp.send_response(stream);
+                 }
+        }
+    }
+}
